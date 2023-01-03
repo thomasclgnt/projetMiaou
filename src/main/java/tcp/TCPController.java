a948f941a90489249ebe859117332c25aea45eb0
Cayla-Ginestet Thomas <caylagin@insa-20549>
2023-01-03 15:17:34 +0100

    TestTCP et TCPController à finir

--- src/main/java/tcp/TCPController.java
+++ src/main/java/tcp/TCPController.java
@@ -2,14 +2,15 @@ package tcp;
 
 import java.io.*;
 
-public class StartSession {
+public class TCPController {
+
+    //rajouter une méthode clienécoute qui place le client en écoute ? => thread
 
     //appeler cette fonction pour lancer TCP dans le main
     //donner en argument user.portTCP
     public static void startSession(int portTCP) throws IOException, InterruptedException {
         TCPServer server = new TCPServer();
         server.start(portTCP);
-
         Thread.sleep(2000);
         server.stop() ;
     }
