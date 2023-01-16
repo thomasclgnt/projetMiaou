7ca888e85ec6169bda3197505dce3996e32409f2
Mecaliff Marie <mecaliff@insa-10577>
2023-01-16 14:02:31 +0100

    début installation fxml

---
+++ src/main/java/frontend/CheckJavaFx.java
@@ -0,0 +1,9 @@
+package frontend;
+import javafx.scene.paint.Color;
+
+public final class CheckJavaFx {
+    public static void main(String[] args) {
+        Color c = Color.RED;
+        System.out.println(c.getRed());
+    }
+}
