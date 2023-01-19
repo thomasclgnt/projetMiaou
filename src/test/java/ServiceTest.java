6fc2f87502a8498c9bcc5137da65d0cbb0ec9ae4
Mecaliff Marie <mecaliff@insa-20658>
2023-01-19 15:03:35 +0100

    debut test udp 2 ordis

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -12,28 +12,40 @@ public class ServiceTest {
         Service serv = new Service() ;
         serv.lancerService();
 
-        User Thomas = new User("TDMKM", "192.168.3.100", 1234);
-        User Marie = new User("marielabest", "192.168.3.101", 1235);
-        UDPController.sendConnexion(Thomas);
+        //User Thomas = new User("TDMKM", "192.168.3.100", 1234);
+        User Marie = new User("marielabest", "192.168.3.101", 1234);
+        //UDPController.sendConnexion(Thomas);
         UDPController.sendConnexion(Marie);
 
-        Thread.sleep(2000);
+        Thread.sleep(20000);
 
-        assertEquals("[TDMKM, 192.168.3.100, 1234 ; \n" +
-                "marielabest, 192.168.3.101, 1235 ; \n" +
+       /* assertEquals("[TDMKM, 192.168.3.100, 1234 ; \n" +
                 "]", serv.getUsers().listToString());
+
+        //assertEquals("[TDMKM, 192.168.3.100, 1234 ; \n" +
+         //       "marielabest, 192.168.3.101, 1235 ; \n" +
+           //     "]", serv.getUsers().listToString());
 
         Thread.sleep(2000);
 
         UDPController.sendNewUsername(Marie, "marie_d_ac");
         Thread.sleep(2000);
-        UDPController.sendDeconnexion(Thomas);
+
+       // UDPController.sendDeconnexion(Thomas);
         Thread.sleep(2000);
-        assertEquals("[marie_d_ac, 192.168.3.101, 1235 ; \n" + "]" , serv.getUsers().listToString());
-        assertNotEquals("[TDMKM, 192.168.3.100, 1234 ; \n" +
-                "marielabest, 192.168.3.101, 1235 ; \n" +
-                "]", serv.getUsers().listToString());
-        Thread.sleep(3000);
+
+        //assertEquals("[marie_d_ac, 192.168.3.101, 1235 ; \n" + "]" , serv.getUsers().listToString());
+        assertEquals("[TDMKM, 192.168.3.100, 1234 ; \n" + "]", serv.getUsers().listToString());
+
+        //assertNotEquals("[TDMKM, 192.168.3.100, 1234 ; \n" +
+          //      "marielabest, 192.168.3.101, 1235 ; \n" +
+            //    "]", serv.getUsers().listToString());
+
+        Thread.sleep(2000);
+
+        assertEquals("[]", serv.getUsers().listToString());
+
+        Thread.sleep(3000); */
         UDPSender.broadcast("end");
 
     }
