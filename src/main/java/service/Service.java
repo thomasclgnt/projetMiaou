3fcd8bfcdc091f0ab8fef1eda853affa189ed7a1
Thomas Cayla Ginestet <caylagin@insa-toulouse.fr>
2023-01-22 19:00:45 +0100

    test tcp 2 ordis

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -32,7 +32,7 @@ public class Service {
                 System.out.println("on est dans le callback0, voici liste des users données par service : \n" + getUsers().listToString());
                 User distant = getUsers().findUser(from.getHostAddress());//vérifier que socket.getInetAddress prend l'adresse distante et pas la notre //renvoie l'user correspondant à l'adresse ip
                 System.out.println("on arrive meme jusque la");
-                User us = getUsers().findUser(IPAddress.getLocalIP());
+                User us = getUsers().findUser(IPAddress.getLocalIP().getHostAddress());
                 System.out.println("on est dans le callback1, voici liste des users données par service : \n" + getUsers().listToString());
 
                 MessageIn msgData = new MessageIn(distant.username, distant.addressIP, us.username, us.addressIP, message, horodatage);
